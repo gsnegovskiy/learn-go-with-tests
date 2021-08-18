4cbad17c6eeaf6a573c5e24d5c6daf799bd19232
Greg Snow <greg.snow@footballindex.co.uk>
2021-08-18 18:37:04 +0300

    refactor SumAll

--- arrays/sum.go
+++ arrays/sum.go
@@ -9,12 +9,21 @@ func Sum(numbers []int) int {
 }
 
 func SumAll(numbersToSum ...[]int) []int {
-	lenghtOfNumbers := len(numbersToSum)
-	sums := make([]int, lenghtOfNumbers)
-
-	for i, numbers := range numbersToSum {
-		sums[i] = Sum(numbers)
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
 	}
-
 	return sums
 }
+
+//func SumAll(numbersToSum ...[]int) []int {
+//	lenghtOfNumbers := len(numbersToSum)
+//	sums := make([]int, lenghtOfNumbers)
+//
+//	for i, numbers := range numbersToSum {
+//		sums[i] = Sum(numbers)
+//	}
+//
+//	return sums
+//}
+//
