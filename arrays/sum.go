bcb40a13e889b604507eebe6a31baa6313b471c2
Greg Snow <greg.snow@footballindex.co.uk>
2021-08-18 18:46:30 +0300

    Sum tails

--- arrays/sum.go
+++ arrays/sum.go
@@ -16,6 +16,19 @@ func SumAll(numbersToSum ...[]int) []int {
 	return sums
 }
 
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
+}
+
 //func SumAll(numbersToSum ...[]int) []int {
 //	lenghtOfNumbers := len(numbersToSum)
 //	sums := make([]int, lenghtOfNumbers)
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -26,9 +26,27 @@ func TestSumAll(t *testing.T) {
 			t.Errorf("got %d want %d, input %v", got, want, allArgs)
 		}
 	}
-	t.Run("Repeat any times", func(t *testing.T) {
+	t.Run("Sum all slices provided", func(t *testing.T) {
 		got := SumAll([]int{1, 2}, []int{0, 9})
 		want := []int{3, 9}
+		assertCorrectMessage(t, got, want, []int{1, 2}, []int{0, 9})
+	})
+}
+func TestSumAllTails(t *testing.T) {
+	assertCorrectMessage := func(t testing.TB, got, want []int, allArgs ...[]int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %d want %d, input %v", got, want, allArgs)
+		}
+	}
+	t.Run("Sum only tails of slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+		assertCorrectMessage(t, got, want, []int{1, 2}, []int{0, 9})
+	})
+	t.Run("Safe sum of empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{0, 9})
+		want := []int{0, 9}
 		assertCorrectMessage(t, got, want, []int{1, 2}, []int{0, 9})
 	})
 }
