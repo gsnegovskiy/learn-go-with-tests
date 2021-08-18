4359880fc73de3d032b5cfa43490be5b7949d950
Greg Snow <greg.snow@footballindex.co.uk>
2021-08-18 18:33:52 +0300

    add sum all method

--- arrays/sum.go
+++ arrays/sum.go
@@ -7,3 +7,14 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	lenghtOfNumbers := len(numbersToSum)
+	sums := make([]int, lenghtOfNumbers)
+
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+
+	return sums
+}
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,6 +1,9 @@
 package arrays
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	assertCorrectMessage := func(t testing.TB, got, want int, numbers []int) {
@@ -9,16 +12,23 @@ func TestSum(t *testing.T) {
 			t.Errorf("got %d want %d, %v", got, want, numbers)
 		}
 	}
-	t.Run("Repeat 5 times", func(t *testing.T) {
-		numbers := []int{1, 2, 3, 4, 5}
-		got := Sum(numbers)
-		want := 15
-		assertCorrectMessage(t, got, want, numbers)
-	})
 	t.Run("Repeat any times", func(t *testing.T) {
 		numbers := []int{1, 2, 3}
 		got := Sum(numbers)
 		want := 6
 		assertCorrectMessage(t, got, want, numbers)
+	})
+}
+func TestSumAll(t *testing.T) {
+	assertCorrectMessage := func(t testing.TB, got, want []int, allArgs ...[]int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %d want %d, input %v", got, want, allArgs)
+		}
+	}
+	t.Run("Repeat any times", func(t *testing.T) {
+		got := SumAll([]int{1, 2}, []int{0, 9})
+		want := []int{3, 9}
+		assertCorrectMessage(t, got, want, []int{1, 2}, []int{0, 9})
 	})
 }
