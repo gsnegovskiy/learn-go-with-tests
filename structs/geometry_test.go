38c9b6aaeb0637afd1c6cfa0fe8cabeadee15a5b
Greg Snow <greg.snow@footballindex.co.uk>
2021-08-21 15:39:11 +0300

    add area method

--- structs/geometry_test.go
+++ structs/geometry_test.go
@@ -16,3 +16,17 @@ func TestPerimeter(t *testing.T) {
 	assertCorrectMessage(t, got, want)
 
 }
+
+func TestArea(t *testing.T) {
+	assertCorrectMessage := func(t testing.TB, got, want float64) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	}
+
+	got := Area(10.0, 10.0)
+	want := 100.0
+	assertCorrectMessage(t, got, want)
+
+}
