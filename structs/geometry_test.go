30703a5438174fc40f003e367fd220a1f94a8526
Greg Snow <greg.snow@footballindex.co.uk>
2021-08-21 15:29:56 +0300

    add perimeter function

---
+++ structs/geometry_test.go
@@ -0,0 +1,18 @@
+package structs
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+
+	assertCorrectMessage := func(t testing.TB, got, want float64) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	}
+
+	got := Perimeter(10.0, 10.0)
+	want := 40.0
+	assertCorrectMessage(t, got, want)
+
+}
