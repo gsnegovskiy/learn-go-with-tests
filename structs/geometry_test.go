c806f96b27b28d7f1ac0155bdc07102ebb9c1463
Greg Snow <greg.snow@footballindex.co.uk>
2021-08-21 16:07:25 +0300

    use method reciever with structs

--- structs/geometry_test.go
+++ structs/geometry_test.go
@@ -10,11 +10,12 @@ func TestPerimeter(t *testing.T) {
 			t.Errorf("got %.2f want %.2f", got, want)
 		}
 	}
-	rectangle := Rectangle{10.0, 10.0}
-	got := Perimeter(rectangle)
-	want := 40.0
-	assertCorrectMessage(t, got, want)
-
+	t.Run("Calculate rectangle perimeter", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		got := Rectangle.Perimeter(rectangle)
+		want := 40.0
+		assertCorrectMessage(t, got, want)
+	})
 }
 
 func TestArea(t *testing.T) {
@@ -24,9 +25,16 @@ func TestArea(t *testing.T) {
 			t.Errorf("got %.2f want %.2f", got, want)
 		}
 	}
-	rectangle := Rectangle{10.0, 10.0}
-	got := Area(rectangle)
-	want := 100.0
-	assertCorrectMessage(t, got, want)
-
+	t.Run("Calculate rectangle area", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		got := Rectangle.Area(rectangle)
+		want := 100.0
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("Calculate circle area", func(t *testing.T) {
+		circle := Circle{100.0}
+		got := Circle.Area(circle)
+		want := 314.1592653589793
+		assertCorrectMessage(t, got, want)
+	})
 }
